1bd4910ff38d8c70b17de8a1916d6a840dda7d62
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-09 12:03:47 -0500

    Create diagnostico.py

---
+++ pages/diagnostico.py
@@ -0,0 +1,9 @@
+st.title('Define tu esencia')
+
+st.selectbox('Elige el rol más demandado a futuro que desees abordar', ['Data Scientist', 'Broker', 'ML Operator'])
+st.multiselect('Selecciona los problemas del planeta que deseas enfrentar', ['Hambre', 'Pobreza', 'Educacion'])
+st.multiselect('Selecciona tus principales pasatiempos', ['Leer', 'Ejercicio', 'Cine'])
+st.text_input('Indícanos actualmente en qué te ganas la vida')
+
+
+st.button('Preparar Mando de Novus Vita exclusivo para mí')
