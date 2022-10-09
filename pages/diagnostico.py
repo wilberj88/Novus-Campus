ae7dd8f3e6fc41686b8973915a785dd6912a6f84
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-09 12:04:24 -0500

    Update diagnostico.py

--- pages/diagnostico.py
+++ pages/diagnostico.py
@@ -1,3 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Campus", page_icon="🏛️")
+
+st.title('Novus Campus 🏛️')
+
+st.header("Define tu meta ✍️")
+st.subheader("Prepárate para alcanzarla 🎯")
+st.subheader("Monitorea Paso a Paso 👣 la Materialización ✅")
+
+
 st.title('Define tu esencia')
 
 st.selectbox('Elige el rol más demandado a futuro que desees abordar', ['Data Scientist', 'Broker', 'ML Operator'])
