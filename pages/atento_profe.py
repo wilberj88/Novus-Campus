1ecbae95cb26a437422ea774b6a172133df4a7bb
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 12:48:38 -0500

    Update atento_profe.py

--- pages/atento_profe.py
+++ pages/atento_profe.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 
 with st.chat_message("user"):
-    st.write("👋Bienvenido a Novus Campus 🏛️, soy tu profe Atento 🤖: dime qué opinas de la siguiente gráfica?")
+    st.write("👋Bienvenido a Novus Campus 🏛️, soy tu profe Atento 🤖: dime ¿qué opinas de la siguiente gráfica?")
     st.bar_chart(np.random.randn(30, 3))
 
 prompt = st.chat_input("Tienes 30 segundos para tu respuesta...")
