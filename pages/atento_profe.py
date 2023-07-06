7f54158646fc174f9d267f69c22a65c8e3ff3959
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 12:39:42 -0500

    Update atento_profe.py

--- pages/atento_profe.py
+++ pages/atento_profe.py
@@ -4,3 +4,7 @@ import numpy as np
 with st.chat_message("user"):
     st.write("👋Bienvenido a Novus Campus 🏛️, soy tu profe Atento 🤖: dime qué opinas de la siguiente gráfica?")
     st.bar_chart(np.random.randn(30, 3))
+
+prompt = st.chat_input("Tienes 30 segundos para tu respuesta")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
