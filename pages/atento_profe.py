b4d6b243bd03c3e4de55c16b189b1b1e71152f8c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 13:56:28 -0500

    Update atento_profe.py

--- pages/atento_profe.py
+++ pages/atento_profe.py
@@ -2,9 +2,16 @@ import streamlit as st
 import numpy as np
 
 st.title('Novus Atento 🤖 en Novus Campus 🏛️')
-with st.chat_message("user"):
-    st.write("👋Bienvenido a Novus Campus 🏛️, soy tu profe Atento 🤖: dime ¿qué opinas de la siguiente gráfica?")
-    st.bar_chart(np.random.randn(30, 3))
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["user"]):
+        st.markdown(message["👋Bienvenido a Novus Campus 🏛️, soy tu profe Atento 🤖: dime ¿qué opinas de la siguiente gráfica"])
+        st.bar_chart(np.random.randn(30, 3))
 
 prompt = st.chat_input("Tienes 30 segundos para tu respuesta...")
 if prompt:
